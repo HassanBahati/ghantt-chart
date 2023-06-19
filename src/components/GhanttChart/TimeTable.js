1f14a9446463f636424b8b18702ee89399768609
HassanBahati <mukisabahati@gmail.com>
2023-06-19 15:36:30 +0300

    feat: add ability to delete task durations - make our task durations deletable by pressing the delete button when the task duration is in focus

--- src/components/GhanttChart/TimeTable.js
+++ src/components/GhanttChart/TimeTable.js
@@ -1,5 +1,13 @@
-import { useState } from 'react';
-import { monthDiff } from '../../helpers/dateFunctions';
+import { useState } from "react";
+import {
+  monthDiff,
+  getDaysInMonth,
+  getDayOfWeek,
+  createFormattedDateFromStr,
+  createFormattedDateFromDate,
+  dayDiff,
+} from "../../helpers/dateFunctions";
+import { months } from "@/utils/constants";
 
 export default function TimeTable({
   timeRange,
@@ -9,33 +17,33 @@ export default function TimeTable({
 }) {
   // for dynamic css styling
   const ganttTimePeriod = {
-    display: 'grid',
-    gridAutoFlow: 'column',
-    gridAutoColumns: 'minmax(30px, 1fr)',
-    outline: '0.5px solid var(--color-outline)',
-    textAlign: 'center',
-    height: 'var(--cell-height)',
+    display: "grid",
+    gridAutoFlow: "column",
+    gridAutoColumns: "minmax(30px, 1fr)",
+    outline: "0.5px solid var(--color-outline)",
+    textAlign: "center",
+    height: "var(--cell-height)",
   };
 
   const ganttTimePeriodSpan = {
-    margin: 'auto',
+    margin: "auto",
   };
 
   const ganttTimePeriodCell = {
-    position: 'relative',
-    outline: '0.5px solid var(--color-outline)',
-    marginTop: '0.5px',
+    position: "relative",
+    outline: "0.5px solid var(--color-outline)",
+    marginTop: "0.5px",
   };
 
   const taskDuration = {
-    position: 'absolute',
-    height: 'calc(var(--cell-height) - 1px)',
-    zIndex: '1',
+    position: "absolute",
+    height: "calc(var(--cell-height) - 1px)",
+    zIndex: "1",
     background:
-      'linear-gradient(90deg, var(--color-primary-light) 0%, var(--color-primary-dark) 100%)',
-    borderRadius: 'var(--border-radius)',
-    boxShadow: '3px 3px 3px rgba(0, 0, 0, 0.05)',
-    cursor: 'move',
+      "linear-gradient(90deg, var(--color-primary-light) 0%, var(--color-primary-dark) 100%)",
+    borderRadius: "var(--border-radius)",
+    boxShadow: "3px 3px 3px rgba(0, 0, 0, 0.05)",
+    cursor: "move",
   };
 
   // creating rows
@@ -58,6 +66,130 @@ export default function TimeTable({
   let taskRows = [];
   let taskRow = [];
 
+  // loops through the number of months in the time period range and creates month, week, and day rows
+  for (let i = 0; i < numMonths; i++) {
+    // create month rows
+    monthRows.push(
+      <div key={i} style={{ ...ganttTimePeriod, outline: "none" }}>
+        <span style={ganttTimePeriodSpan}>
+          {months[month.getMonth()] + " " + month.getFullYear()}
+        </span>
+      </div>
+    );
+
+    // create day and week rows
+    const numDays = getDaysInMonth(month.getFullYear(), month.getMonth() + 1);
+    const currYear = month.getFullYear();
+    const currMonth = month.getMonth() + 1;
+
+    for (let j = 1; j <= numDays; j++) {
+      dayRow.push(
+        <div key={j} style={{ ...ganttTimePeriod, outline: "none" }}>
+          <span style={ganttTimePeriodSpan}>{j}</span>
+        </div>
+      );
+
+      weekRow.push(
+        <div key={j} style={{ ...ganttTimePeriod, outline: "none" }}>
+          <span style={{ ...ganttTimePeriodSpan, color: "#3E455B" }}>
+            {getDayOfWeek(currYear, currMonth - 1, j - 1)}
+          </span>
+        </div>
+      );
+    }
+
+    dayRows.push(
+      <div key={i} style={{ ...ganttTimePeriod, outline: "none" }}>
+        {dayRow}
+      </div>
+    );
+
+    weekRows.push(
+      <div key={i} style={{ ...ganttTimePeriod, outline: "none" }}>
+        {weekRow}
+      </div>
+    );
+
+    dayRow = [];
+    weekRow = [];
+    month.setMonth(month.getMonth() + 1);
+  }
+
+  // create task rows
+  if (tasks) {
+    tasks.forEach((task) => {
+      let mnth = new Date(startMonth);
+      for (let i = 0; i < numMonths; i++) {
+        const curYear = mnth.getFullYear();
+        const curMonth = mnth.getMonth() + 1;
+
+        const numDays = getDaysInMonth(curYear, curMonth);
+
+        for (let j = 1; j <= numDays; j++) {
+          // color weekend cells differently
+          const dayOfTheWeek = getDayOfWeek(curYear, curMonth - 1, j - 1);
+          // add task and date data attributes
+          const formattedDate = createFormattedDateFromStr(
+            curYear,
+            curMonth,
+            j
+          );
+
+          taskRow.push(
+            <div
+              key={`${task.id}-${j}`}
+              style={{
+                ...ganttTimePeriodCell,
+                backgroundColor:
+                  dayOfTheWeek === "S" ? "var(--color-tertiary)" : "#fff",
+              }}
+              data-task={task?.id}
+              data-date={formattedDate}
+            >
+              {taskDurations.map((el, i) => {
+                if (el?.task === task?.id && el?.start === formattedDate) {
+                  return (
+                    <div
+                      onKeyDown={(e) => deleteTaskDuration(e, el?.id)}
+                      key={`${i}-${el?.id}`}
+                      tabIndex="0"
+                      style={{
+                        ...taskDuration,
+                        width: `calc(${dayDiff(
+                          el?.start,
+                          el?.end
+                        )} * 100% - 1px)`,
+                      }}
+                    ></div>
+                  );
+                }
+              })}
+            </div>
+          );
+        }
+
+        taskRows.push(
+          <div key={`${i}-${task?.id}`} style={ganttTimePeriod}>
+            {taskRow}
+          </div>
+        );
+
+        taskRow = [];
+        mnth.setMonth(mnth.getMonth() + 1);
+      }
+    });
+  }
+
+  function deleteTaskDuration(e, id) {
+    if (e.key === "Delete" || e.key === "Backspace") {
+      // update taskDurations
+      const newTaskDurations = taskDurations.filter(
+        (taskDuration) => taskDuration.id !== id
+      );
+      // update state (if data on backend - make API request to update data)
+      setTaskDurations(newTaskDurations);
+    }
+  }
   return (
     <div
       id="gantt-grid-container__time"
@@ -69,10 +201,10 @@ export default function TimeTable({
       <div
         id="gantt-time-period-cell-container"
         style={{
-          gridColumn: '1/-1',
-          display: 'grid',
+          gridColumn: "1/-1",
+          display: "grid",
           gridTemplateColumns: `repeat(${numMonths}, 1fr)`,
-          paddingLeft: '0.5px',
+          paddingLeft: "0.5px",
         }}
       >
         {taskRows}

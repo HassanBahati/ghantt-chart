a7811c1feb24850af633ea694d546828cea33b16
HassanBahati <mukisabahati@gmail.com>
2023-06-19 15:58:14 +0300

    feat: create task functionality - enable user to create a new task

--- src/components/GhanttChart/AddTask.js
+++ src/components/GhanttChart/AddTask.js
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import AddButton from "./AddButton";
 
 export default function AddTask({ setTasks, tasks }) {
-  const [task, setTask] = useState("");
+  const [task, setTask] = useState();
 
   function onChange(e) {
     setTask(e.target.value);
@@ -10,6 +10,22 @@ export default function AddTask({ setTasks, tasks }) {
 
   function handleSubmit(e) {
     e.preventDefault();
+    setTasks((prevState) => {
+      const newState = prevState;
+      // find largest task number, add 1 for new task - else could end up with tasks with same id
+      const maxIdVal = prevState.reduce(function (a, b) {
+        return Math.max(a, b.id);
+      }, -Infinity);
+
+      // create new task
+      newState.push({
+        id: isFinite(maxIdVal) ? maxIdVal + 1 : 1,
+        name: task,
+      });
+
+      return [...newState];
+    });
+
     setTask("");
   }
 
